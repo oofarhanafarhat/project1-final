4d8c9b2720193eada43d7976f4a6e1b4f1f02380
oofarhanafarhat <khnkaneez@gmail.com>
2024-12-29 00:34:06 +0500

    Add files via upload
    add

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Blogs from "@/component/Blogs";
+import Hero from "@/component/Hero";
+import Insta from "@/component/Insta";
+import Nav from "@/component/Nav";
+import Pick from "@/component/Pick";
+import Pick1 from "@/component/Pick1";
+import Title from "@/component/Title";
+
+
+export default function Home() {
+  return (
+    
+      
+<div >
+  < Nav/>
+<Title/>
+<Hero/>
+< Pick/>
+<Pick1/>
+< Blogs/>
+<Insta/>
+</div>
+  );
+}
